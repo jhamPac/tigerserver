1b363841b5412610eb139ce839f9b80d1f214682
jhamPac <cloud@kevia.me>
2020-02-16 14:11:16 -0700

    basic red test

---
+++ tigerserver.go
@@ -0,0 +1,8 @@
+package tigerserver
+
+import "net/http"
+
+// TigerServer initiates an instance of a power server.
+func TigerServer(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ tigerserver_test.go
@@ -0,0 +1,23 @@
+package tigerserver
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGETPlayers(t *testing.T) {
+	t.Run("returns Casio's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Casio/", nil)
+		response := httptest.NewRecorder()
+
+		TigerServer(response, request)
+
+		got := response.Body.String()
+		want := "20"
+
+		if got != want {
+			t.Errorf("got %q, wanted %q", got, want)
+		}
+	})
+}
