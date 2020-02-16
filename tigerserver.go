1b983b11f22922a56e076b57a570782523e5ec10
jhamPac <cloud@kevia.me>
2020-02-16 14:12:59 -0700

    passes test

--- tigerserver.go
+++ tigerserver.go
@@ -1,8 +1,11 @@
 package tigerserver
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // TigerServer initiates an instance of a power server.
 func TigerServer(w http.ResponseWriter, r *http.Request) {
-
+	fmt.Fprint(w, "20")
 }
