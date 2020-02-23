9c136d5bb25e94007d349190c5fd6f4d62809e40
jhamPac <cloud@kevia.me>
2020-02-23 16:22:43 -0700

    ReadSeeker vs Read

--- file_system_playerstore.go
+++ file_system_playerstore.go
@@ -6,11 +6,12 @@ import (
 
 // FileSystemPlayerStore implements the PlayerStore interface for TigerServer
 type FileSystemPlayerStore struct {
-	database io.Reader
+	database io.ReadSeeker
 }
 
 // GetLeague returns a slice of type Player
 func (f *FileSystemPlayerStore) GetLeague() []Player {
+	f.database.Seek(0, 0)
 	league, _ := NewLeague(f.database)
 	return league
 }
