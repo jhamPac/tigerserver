65ad9c947e12d83819d1d331f7ab8dd3726bd9b5
jhamPac <cloud@kevia.me>
2020-02-23 16:12:08 -0700

    pass test with hardcoded values

--- file_system_playerstore.go
+++ file_system_playerstore.go
@@ -1,3 +1,18 @@
 package tigerserver
 
-type FileSystemPlayerStore struct{}
+import (
+	"encoding/json"
+	"io"
+)
+
+// FileSystemPlayerStore implements the PlayerStore interface for TigerServer
+type FileSystemPlayerStore struct {
+	database io.Reader
+}
+
+// GetLeague returns a slice of type Player
+func (f *FileSystemPlayerStore) GetLeague() []Player {
+	var league []Player
+	json.NewDecoder(f.database).Decode(&league)
+	return league
+}
--- file_system_playerstore_test.go
+++ file_system_playerstore_test.go
@@ -9,7 +9,8 @@ func TestFileSystemStore(t *testing.T) {
 	t.Run("/league from a reader", func(t *testing.T) {
 		database := strings.NewReader(`[
 			{"Name": "Cleo", "Wins": 10},
-			{"Name": "Chris", "Wins": 33}]`)
+			{"Name": "Chris", "Wins": 30
+			}]`)
 
 		store := FileSystemPlayerStore{database}
 
