92457be6a4793153f74e604fb1762ff21778bedd
jhamPac <cloud@kevia.me>
2020-03-05 09:17:30 -0700

    separated DB initialization into its own function

--- file_system_playerstore.go
+++ file_system_playerstore.go
@@ -15,16 +15,10 @@ type FileSystemPlayerStore struct {
 
 // NewFileSystemPlayerStore is a constructor for creating new FileSystemPlayerStore
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
-	file.Seek(0, 0)
-	info, err := file.Stat()
+	err := initializePlayerDBFile(file)
 
 	if err != nil {
-		return nil, fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
-	}
-
-	if info.Size() == 0 {
-		file.Write([]byte("[]"))
-		file.Seek(0, 0)
+		return nil, fmt.Errorf("problem loading player store from file %s, %v", file.Name(), err)
 	}
 
 	league, err := NewLeague(file)
@@ -34,6 +28,22 @@ func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 	}
 
 	return &FileSystemPlayerStore{database: json.NewEncoder(&tape{file}), league: league}, nil
+}
+
+func initializePlayerDBFile(file *os.File) error {
+	file.Seek(0, 0)
+	info, err := file.Stat()
+
+	if err != nil {
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, 0)
+	}
+
+	return nil
 }
 
 // GetLeague returns a slice of type Player
