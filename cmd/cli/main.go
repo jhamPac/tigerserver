e16f89be944790da122d2272836cbaa4ed6e1a35
jhamPac <cloud@kevia.me>
2020-03-15 14:01:26 -0700

    updated cli main.go to use new FileSystemPlayerStore func

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -11,20 +11,15 @@ import (
 const dbFileName = "game.db.json"
 
 func main() {
+	store, close, err := ts.FileSystemPlayerStoreFromFile(dbFileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer close()
+
 	fmt.Println("Let's enter the MATRIX")
 	fmt.Println("Type {Name} wins to record a win")
-
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
-
-	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
-	}
-
-	store, err := ts.NewFileSystemPlayerStore(db)
-
-	if err != nil {
-		log.Fatalf("problem creating file system player store %v", err)
-	}
 
 	game := ts.NewCLI(store, os.Stdin)
 	game.PlayPoker()
