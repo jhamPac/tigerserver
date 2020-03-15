3e8069c6e11f4c5c88c26d28cc8baa5974c89574
jhamPac <cloud@kevia.me>
2020-03-15 15:45:59 -0700

    added a test for the blind alert function

--- cli_test.go
+++ cli_test.go
@@ -24,4 +24,17 @@ func TestCLI(t *testing.T) {
 
 		assertPlayerWin(t, store, "Bishop")
 	})
+
+	t.Run("it schedules printing of blind values", func(t *testing.T) {
+		in := strings.NewReader("Sabertooth wins\n")
+		store := &StubPlayerStore{}
+		blindAlerter := &SpyBlindAlerter{}
+
+		cli := NewCLI(store, in, blindAlerter)
+		cli.PlayPoker()
+
+		if len(blindAlerter.alerts) != {
+			t.Fatal("expected a blind alert to be scheduled")
+		}
+	})
 }
