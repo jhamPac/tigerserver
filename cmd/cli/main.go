c2ed36abcc2a9b2d17d91051f1a164b6bb8e3698
jhamPac <cloud@kevia.me>
2020-03-15 13:25:05 -0700

    building a program using CLI

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	ts "github.com/jhampac/tigerserver"
+)
+
+const dbFileName = "game.db.json"
 
 func main() {
-	fmt.Println("this is the cli")
+	fmt.Println("Let's enter the MATRIX")
+	fmt.Println("Type {Name} wins to record a win")
+
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+
+	store, err := ts.NewFileSystemPlayerStore(db)
+
+	if err != nil {
+		log.Fatalf("problem creating file system player store %v", err)
+	}
+
+	game := ts.CLI{store, os.Stdin}
+	game.PlayPoker()
 }
