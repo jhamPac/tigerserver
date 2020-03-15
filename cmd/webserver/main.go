3c981dfabd505f92f329df19e9f6a932296d2bef
jhamPac <cloud@kevia.me>
2020-03-15 14:03:13 -0700

    updated server interface to use new FS func

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	ts "github.com/jhampac/tigerserver"
 )
@@ -11,17 +10,13 @@ import (
 const dbFileName = "game.db.json"
 
 func main() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	store, close, err := ts.FileSystemPlayerStoreFromFile(dbFileName)
 
 	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
+		log.Fatal(err)
 	}
 
-	store, err := ts.NewFileSystemPlayerStore(db)
-
-	if err != nil {
-		log.Fatalf("problem creating file system player store, %v", err)
-	}
+	defer close()
 
 	server := ts.CreateTigerServer(store)
 
