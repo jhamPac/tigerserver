1f20695af399750525b750ff211a4fce092774eb
jhamPac <cloud@kevia.me>
2020-03-17 11:46:52 -0700

    table driven tests for blind times failed as expected

--- cli_test.go
+++ cli_test.go
@@ -1,8 +1,10 @@
 package tigerserver
 
 import (
+	"fmt"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestCLI(t *testing.T) {
@@ -35,6 +37,52 @@ func TestCLI(t *testing.T) {
 
 		if len(blindAlerter.alerts) != 1 {
 			t.Fatal("expected a blind alert to be scheduled")
+		}
+	})
+
+	t.Run("it schedules printing of blind values", func(t *testing.T) {
+		in := strings.NewReader("Storm wins\n")
+		store := &StubPlayerStore{}
+		blindAlerter := &SpyBlindAlerter{}
+
+		cli := NewCLI(store, in, blindAlerter)
+		cli.PlayPoker()
+
+		cases := []struct {
+			expectedScheduleTime time.Duration
+			expectedAmount       int
+		}{
+			{0 * time.Second, 100},
+			{10 * time.Second, 200},
+			{20 * time.Second, 300},
+			{30 * time.Minute, 400},
+			{40 * time.Minute, 500},
+			{50 * time.Minute, 600},
+			{60 * time.Minute, 800},
+			{70 * time.Minute, 1000},
+			{80 * time.Minute, 2000},
+			{90 * time.Minute, 4000},
+			{100 * time.Minute, 8000},
+		}
+
+		for i, c := range cases {
+			t.Run(fmt.Sprintf("%d scheduled for %v", c.expectedAmount, c.expectedScheduleTime), func(t *testing.T) {
+				if len(blindAlerter.alerts) <= 1 {
+					t.Fatalf("alert %d was not scheduled %v", i, blindAlerter.alerts)
+				}
+
+				alert := blindAlerter.alerts[i]
+
+				amountGot := alert.amount
+				if amountGot != c.expectedAmount {
+					t.Errorf("got amount %d but wanted %d", amountGot, c.expectedAmount)
+				}
+
+				gotScheduledTime := alert.scheduledAt
+				if gotScheduledTime != c.expectedScheduleTime {
+					t.Errorf("got scheduled time of %v but wanted %v", gotScheduledTime, c.expectedScheduleTime)
+				}
+			})
 		}
 	})
 }
