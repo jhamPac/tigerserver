b14adcbe3e9b7aa3605dd9ade6787385572a9dbb
jhamPac <cloud@kevia.me>
2020-03-17 11:52:33 -0700

    still failing mmmmm

--- cli.go
+++ cli.go
@@ -26,7 +26,14 @@ func NewCLI(store PlayerStore, i io.Reader, alerter BlindAlerter) *CLI {
 
 // PlayPoker initiates a game
 func (c *CLI) PlayPoker() {
-	c.alerter.ScheduleAlertAt(5*time.Second, 100)
+	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
+	blindTime := 0 * time.Second
+
+	for _, blind := range blinds {
+		c.alerter.ScheduleAlertAt(blindTime, blind)
+		blindTime = blindTime + 10*time.Minute
+	}
+
 	userInput := c.readLine()
 	c.store.RecordWin(extractWinner(userInput))
 }
