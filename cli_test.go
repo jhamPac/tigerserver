42d0e96b3357aef6a6995d346e5b0e5c4f39a88b
jhamPac <cloud@kevia.me>
2020-03-19 08:29:45 -0700

    going to change the hard coded time schedules

--- cli_test.go
+++ cli_test.go
@@ -81,8 +81,11 @@ func TestCLI(t *testing.T) {
 
 	t.Run("it prompts the user to enter the number of players", func(t *testing.T) {
 		store := &StubPlayerStore{}
+		in := strings.NewReader("7\n")
 		stdout := &bytes.Buffer{}
-		cli := NewCLI(store, dummyStdin, stdout, dummyBlindAlerter)
+		blindAlerter := &SpyBlindAlerter{}
+
+		cli := NewCLI(store, in, stdout, blindAlerter)
 		cli.PlayPoker()
 
 		got := stdout.String()
@@ -90,6 +93,24 @@ func TestCLI(t *testing.T) {
 
 		if got != want {
 			t.Errorf("got %q but wanted %q", got, want)
+		}
+
+		cases := []scheduledAlert{
+			{0 * time.Second, 100},
+			{12 * time.Minute, 200},
+			{24 * time.Minute, 300},
+			{36 * time.Minute, 400},
+		}
+
+		for i, want := range cases {
+			t.Run(fmt.Sprint(want), func(t *testing.T) {
+				if len(blindAlerter.alerts) <= i {
+					t.Fatalf("alert %d was not scheduled %v", i, blindAlerter.alerts)
+				}
+
+				got := blindAlerter.alerts[i]
+				assertScheduledAlert(t, got, want)
+			})
 		}
 	})
 }
