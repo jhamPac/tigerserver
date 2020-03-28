902ae7415c0682ecdbb09bc72e2060f627c9b24f
jhamPac <cloud@kevia.me>
2020-03-28 13:38:35 -0700

    testing blind values in game testing

---
+++ game_test.go
@@ -0,0 +1,46 @@
+package tigerserver
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestGame_Start(t *testing.T) {
+	t.Run("schedules alerts on game start for 5 players", func(t *testing.T) {
+		blindAlerter := &SpyBlindAlerter{}
+		store := &StubPlayerStore{}
+		game := NewGame(blindAlerter, store)
+
+		game.Start(5)
+
+		cases := []scheduledAlert{
+			{at: 0 * time.Second, amount: 100},
+			{at: 10 * time.Minute, amount: 200},
+			{at: 20 * time.Minute, amount: 300},
+			{at: 30 * time.Minute, amount: 400},
+			{at: 40 * time.Minute, amount: 500},
+			{at: 50 * time.Minute, amount: 600},
+			{at: 60 * time.Minute, amount: 800},
+			{at: 70 * time.Minute, amount: 1000},
+			{at: 80 * time.Minute, amount: 2000},
+			{at: 90 * time.Minute, amount: 4000},
+			{at: 100 * time.Minute, amount: 8000},
+		}
+
+		checkSchedulingCases(cases, t, blindAlerter)
+	})
+}
+
+func checkSchedulingCases(cases []scheduledAlert, t *testing.T, blindAlerter *SpyBlindAlerter) {
+	for i, want := range cases {
+		t.Run(fmt.Sprint(want), func(t *testing.T) {
+			if len(blindAlerter.alerts) <= 1 {
+				t.Fatalf("alert %d was not scheduled %v", i, blindAlerter.alerts)
+			}
+
+			got := blindAlerter.alerts[i]
+			assertScheduledAlert(t, got, want)
+		})
+	}
+}
