a6c2aacf620e49ec7f768335c76c419e6e2d0380
jhamPac <cloud@kevia.me>
2020-03-28 13:29:03 -0700

    moving blindtests to Game tests

--- cli_test.go
+++ cli_test.go
@@ -2,10 +2,8 @@ package tigerserver
 
 import (
 	"bytes"
-	"fmt"
 	"strings"
 	"testing"
-	"time"
 )
 
 var dummyBlindAlerter = &SpyBlindAlerter{}
@@ -48,77 +46,6 @@ func TestCLI(t *testing.T) {
 
 		if len(blindAlerter.alerts) <= 1 {
 			t.Fatal("expected a blind alert to be scheduled")
-		}
-	})
-
-	t.Run("it schedules printing of blind values", func(t *testing.T) {
-		in := strings.NewReader("Storm wins\n")
-		store := &StubPlayerStore{}
-		blindAlerter := &SpyBlindAlerter{}
-		game := NewGame(blindAlerter, store)
-
-		cli := NewCLI(in, dummyStdout, game)
-		cli.PlayPoker()
-
-		cases := []scheduledAlert{
-			{0 * time.Second, 100},
-			{10 * time.Minute, 200},
-			{20 * time.Minute, 300},
-			{30 * time.Minute, 400},
-			{40 * time.Minute, 500},
-			{50 * time.Minute, 600},
-			{60 * time.Minute, 800},
-			{70 * time.Minute, 1000},
-			{80 * time.Minute, 2000},
-			{90 * time.Minute, 4000},
-			{100 * time.Minute, 8000},
-		}
-
-		for i, want := range cases {
-			t.Run(fmt.Sprint(want), func(t *testing.T) {
-				if len(blindAlerter.alerts) <= i {
-					t.Fatalf("alert %d was not scheduled %v", i, blindAlerter.alerts)
-				}
-
-				got := blindAlerter.alerts[i]
-				assertScheduledAlert(t, got, want)
-			})
-		}
-	})
-
-	t.Run("it prompts the user to enter the number of players", func(t *testing.T) {
-		store := &StubPlayerStore{}
-		in := strings.NewReader("7\n")
-		stdout := &bytes.Buffer{}
-		blindAlerter := &SpyBlindAlerter{}
-		game := NewGame(blindAlerter, store)
-
-		cli := NewCLI(in, stdout, game)
-		cli.PlayPoker()
-
-		got := stdout.String()
-		want := PlayerPrompt
-
-		if got != want {
-			t.Errorf("got %q but wanted %q", got, want)
-		}
-
-		cases := []scheduledAlert{
-			{0 * time.Second, 100},
-			{12 * time.Minute, 200},
-			{24 * time.Minute, 300},
-			{36 * time.Minute, 400},
-		}
-
-		for i, want := range cases {
-			t.Run(fmt.Sprint(want), func(t *testing.T) {
-				if len(blindAlerter.alerts) <= i {
-					t.Fatalf("alert %d was not scheduled %v", i, blindAlerter.alerts)
-				}
-
-				got := blindAlerter.alerts[i]
-				assertScheduledAlert(t, got, want)
-			})
 		}
 	})
 }
