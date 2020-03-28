f073a028cd4cff89db094823f42941aee72b0d97
jhamPac <cloud@kevia.me>
2020-03-28 13:56:45 -0700

    added an interface for game; now creating the concrete implementation

--- game.go
+++ game.go
@@ -1,32 +1,7 @@
 package tigerserver
 
-import "time"
-
-// Game defines the function and boundry of the game being played
-type Game struct {
-	alerter BlindAlerter
-	store   PlayerStore
-}
-
-// NewGame creates and returns a Game struct
-func NewGame(alerter BlindAlerter, store PlayerStore) *Game {
-	return &Game{alerter: alerter, store: store}
-}
-
-// Start invokes a game and blindalerter call
-func (g *Game) Start(numberOfPlayers int) {
-	blindIncrement := time.Duration(5+numberOfPlayers) * time.Minute
-
-	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
-	blindTime := 0 * time.Second
-
-	for _, blind := range blinds {
-		g.alerter.ScheduleAlertAt(blindTime, blind)
-		blindTime = blindTime + blindIncrement
-	}
-}
-
-// Finish ends a game with a winner
-func (g *Game) Finish(winner string) {
-	g.store.RecordWin(winner)
+// Game any type that allows CLI to play a game
+type Game interface {
+	Start(numberOfPlayers int)
+	Finish(winner string)
 }
