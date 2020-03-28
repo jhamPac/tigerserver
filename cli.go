453517bcb42d74155948bfba5e14e6d735a1bde6
jhamPac <cloud@kevia.me>
2020-03-28 11:25:12 -0700

    refactored cli to take Game as a dependecy

--- cli.go
+++ cli.go
@@ -13,29 +13,28 @@ const PlayerPrompt = "Please enter the number of player: "
 
 // CLI can make calls to the server via terminal client
 type CLI struct {
-	store   PlayerStore
-	in      *bufio.Scanner
-	out     io.Writer
-	alerter BlindAlerter
+	in   *bufio.Scanner
+	out  io.Writer
+	game Game
 }
 
 // NewCLI factory function for object
-func NewCLI(store PlayerStore, i io.Reader, o io.Writer, alerter BlindAlerter) *CLI {
-	return &CLI{store: store, in: bufio.NewScanner(i), out: o, alerter: alerter}
+func NewCLI(i io.Reader, o io.Writer, g Game) *CLI {
+	return &CLI{in: bufio.NewScanner(i), out: o, game: g}
 }
 
 // PlayPoker initiates a game
 func (c *CLI) PlayPoker() {
 	fmt.Fprint(c.out, PlayerPrompt)
-	numberOfPlayersInput := cli.readLine()
+	numberOfPlayersInput := c.readLine()
 	numberOfPlayers, _ := strconv.Atoi(strings.Trim(numberOfPlayersInput, "\n"))
 
-	cli.game.Start(numberOfPlayers)
+	c.game.Start(numberOfPlayers)
 
-	winnerInput := cli.readLine()
+	winnerInput := c.readLine()
 	winner := extractWinner(winnerInput)
 
-	cli.game.Finish(winner)
+	c.game.Finish(winner)
 }
 
 func extractWinner(userInput string) string {
