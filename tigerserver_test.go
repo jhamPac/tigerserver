c712b26e6993837aff4863dffa2a70f4c2ef4b78
jhamPac <cloud@kevia.me>
2020-03-30 14:19:57 -0700

    new game route magically works

--- tigerserver_test.go
+++ tigerserver_test.go
@@ -98,3 +98,17 @@ func TestLeague(t *testing.T) {
 
 	})
 }
+
+func TestGame(t *testing.T) {
+	t.Run("GET /game returns 200", func(t *testing.T) {
+		store := &StubPlayerStore{}
+		server := CreateTigerServer(store)
+
+		request, _ := http.NewRequest(http.MethodGet, "/game", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusOK)
+	})
+}
