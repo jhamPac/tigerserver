afc66a686c4760a4e955e9707f70d7738cf3c071
jhamPac <cloud@kevia.me>
2020-04-02 13:44:26 -0700

    writing new ws test

--- tigerserver_test.go
+++ tigerserver_test.go
@@ -3,11 +3,7 @@ package tigerserver
 import (
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
-	"time"
-
-	"github.com/gorilla/websocket"
 )
 
 func TestGETPlayers(t *testing.T) {
@@ -116,26 +112,7 @@ func TestGame(t *testing.T) {
 		assertStatus(t, response.Code, http.StatusOK)
 	})
 
-	t.Run("when we get a message over a websocket it is a winner of a game", func(t *testing.T) {
-		store := &StubPlayerStore{}
-		winner := "Beast"
-		ts, _ := CreateTigerServer(store)
-		server := httptest.NewServer(ts)
-		defer server.Close()
+	t.Run("start a game with 3 players and declare Cable the winner", func(t *testing.T) {
 
-		wsURL := "ws" + strings.TrimPrefix(server.URL, "http") + "/ws"
-
-		ws, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
-		if err != nil {
-			t.Fatalf("could not open a ws connection on %s %v", wsURL, err)
-		}
-		defer ws.Close()
-
-		if err := ws.WriteMessage(websocket.TextMessage, []byte(winner)); err != nil {
-			t.Fatalf("could not send message over ws connection %v", err)
-		}
-
-		time.Sleep(10 * time.Millisecond)
-		assertPlayerWin(t, store, winner)
 	})
 }
